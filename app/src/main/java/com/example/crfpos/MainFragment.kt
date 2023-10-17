44f4a8ccb3efc7b08db6941e99087fbc6eddfe9f
kitakkun <48154936+kitakkun@users.noreply.github.com>
2023-10-17 12:24:51 +0900

    hide ActionBar for SalesFragment

--- app/src/main/java/com/example/crfpos/MainFragment.kt
+++ app/src/main/java/com/example/crfpos/MainFragment.kt
@@ -2,6 +2,7 @@ package com.example.crfpos
 
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.crfpos.databinding.MainFragmentBinding
@@ -25,6 +26,14 @@ class MainFragment : Fragment(R.layout.main_fragment) {
 
         binding.buttonRecords.setOnClickListener {
             findNavController().navigate(R.id.action_mainFragment_to_recordsFragment)
+        }
+
+        // salesFragmentだけComposeのTopAppBarを出すので、ActionBarを隠す
+        findNavController().addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.salesFragment -> (activity as? AppCompatActivity)?.supportActionBar?.hide()
+                else -> (activity as? AppCompatActivity)?.supportActionBar?.show()
+            }
         }
     }
 }
